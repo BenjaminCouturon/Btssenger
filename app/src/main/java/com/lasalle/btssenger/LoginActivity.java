b983db1e4f684366d04c280ec57e082d215f69f0
benjamin.couturon <benji.couturon@netcourrier.com>
2019-12-10 13:28:29 +0100

    LoginActivity

--- app/src/main/java/com/lasalle/btssenger/LoginActivity.java
+++ app/src/main/java/com/lasalle/btssenger/LoginActivity.java
@@ -1,4 +1,16 @@
 package com.lasalle.btssenger;
 
-public class LoginActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.login_activity);
+    }
 }
