843fa7770e68c76c33e8be52004301634642968a
bluephoenix2507 <theo2507@hotmail.fr>
2020-01-21 16:05:46 +0100

    Prend en compte le http via le dossier xml et le AndroidManifest.xml

---
+++ app/src/main/java/com/lasalle/btssenger/Message.java
@@ -0,0 +1,43 @@
+package com.lasalle.btssenger;
+
+
+import java.util.List;
+
+public class Message {
+    private String msg;
+    private String method;
+    private String id;
+    private List<Param> params;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<Param> getParams() {
+        return params;
+    }
+
+    public void setParams(List<Param> params) {
+        this.params = params;
+    }
+}
---
+++ app/src/main/java/com/lasalle/btssenger/Param.java
@@ -0,0 +1,31 @@
+package com.lasalle.btssenger;
+
+public class Param {
+   private String _id;
+   private String Rid;
+   private String msg;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getRid() {
+        return Rid;
+    }
+
+    public void setRid(String rid) {
+        Rid = rid;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
